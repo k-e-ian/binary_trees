5eef1e0cc3ff96e8e2558fa636eddd18b5c271e7
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-12-16 23:34:20 +0300

    New Node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node = creates a binary tree node
+ * @parent: a pointer to parent of node
+ * @value: value to input in node
+ * Return: NULL if error occurs otherwise pointer to new node
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	new = malloc(sizeof(binary_tree_t));
+	if (new ==  NULL)
+		return (NULL);
+
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
+
+	return (new);
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# 0x1D. C - Binary trees
+# C
+## Group project
+## Algorithm
+## Data structure
+ `By`: `Alexandre Gautier`
+ `Weight`: `5`
+ Project to be done in teams of 2 people (your team: `Ian Kitembe`)
+ Ongoing second chance project - started `Dec 12, 2022 6:00 AM`, must end by `Dec 17, 2022 6:00 AM`
+ An auto review will be launched at the deadline
+
+
