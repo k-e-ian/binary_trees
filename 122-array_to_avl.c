7ec93b47f7540817fab892a849437f98554f9fd2
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-02-20 13:05:05 +0300

    build an avl tree from an array

--- 122-array_to_avl.c
+++ 122-array_to_avl.c
@@ -10,10 +10,14 @@
  */
 avl_t *array_to_avl(int *array, size_t size)
 {
-	unsigned int i;
+	size_t i;
 	avl_t *root = NULL;
-
-	for (i = 0; i < size; i++)
+	
+	if (!array || size == 0)
+		return (NULL);
+	
+	root = avl_insert(&root, array[0]);
+	for (i = 1; i < size; i++)
 		avl_insert(&root, array[i]);
 
 	return (root);
