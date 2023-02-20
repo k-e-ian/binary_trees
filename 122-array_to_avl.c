62f8dde20b21ac2ea604527332c3ffe240e59998
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-02-20 13:08:09 +0300

    betty

--- 122-array_to_avl.c
+++ 122-array_to_avl.c
@@ -12,10 +12,10 @@ avl_t *array_to_avl(int *array, size_t size)
 {
 	size_t i;
 	avl_t *root = NULL;
-	
+
 	if (!array || size == 0)
 		return (NULL);
-	
+
 	root = avl_insert(&root, array[0]);
 	for (i = 1; i < size; i++)
 		avl_insert(&root, array[i]);
